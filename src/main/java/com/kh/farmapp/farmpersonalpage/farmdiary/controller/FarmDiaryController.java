907adc88b9ada083f02c6bdcd7e0e0d16e5a082f
leekisun1029 <70136063+leekisun1029@users.noreply.github.com>
2020-08-26 10:13:59 +0900

    create farm-diary (#22)
    농장 일기 코드 합치기 완료

--- src/main/java/com/kh/farmapp/farmpersonalpage/farmdiary/controller/FarmDiaryController.java
+++ src/main/java/com/kh/farmapp/farmpersonalpage/farmdiary/controller/FarmDiaryController.java
@@ -1,48 +1,111 @@
 package com.kh.farmapp.farmpersonalpage.farmdiary.controller;
 
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.farmapp.farmpersonalpage.farmdiary.model.service.FarmDiaryService;
+
+import common.dto.FarmDiary;
 
 
 @Controller
 public class FarmDiaryController {
 	
 	@Autowired
-	private FarmDiaryService farmdiaryService;
+	FarmDiaryService farmdiaryService;
+
+	//농장 일기 작성 화면
+	@RequestMapping("/farmdiary/farmdiarywrite.do")
+	public void farmdiaryWrite() {
+		System.out.println("wrtie 페이지 접속 완료");
+		
+	}
 	
-//	@RequestMapping("farmdiary/farmdiarylist.do")
-//	public ModelAndView farmdiaryList() {
+	//농장 일기 작성
+	@RequestMapping("/diary/diarywrite.do")
+	public String diaryWrite(
+			@RequestParam Map<String, Object> commandMap) {
+
+		farmdiaryService.writeFarmDiary(commandMap);
+		
+		
+		return "redirect:/farmdiary/farmdiarylist.do";
+	}
+	
+	//농장일기 리스트
+	@RequestMapping("/farmdiary/farmdiarylist.do")
+	public ModelAndView farmdiaryList(@RequestParam(required = false, defaultValue = "1") int cPage) {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		int cntPerPage = 10;
+		
+		Map<String,Object> res = farmdiaryService.selectFarmDiaryList(cPage, cntPerPage);
+		mav.addObject("paging", res.get("paging"));
+		mav.addObject("list", res);
+		mav.setViewName("farmdiary/farmdiarylist");
+
+		
+		
+		
+		return mav;
+	}
+	
+	@RequestMapping("/farmdiary/farmdiarydetail.do")
+	public String farmdiaryDetail(FarmDiary farmdiary, Model model) {
+		
+		System.out.println("상세페이지 접속완료");
+		System.out.println(farmdiary);
+		
+		model.addAttribute("detail", farmdiaryService.selectFarmDiaryDetail(farmdiary.getFarmDiaryNo()));
+		
+		
+//		Map<String,Object> farmdiary = farmdiaryService.selectFarmDiaryDetail(dailyNo);
+	
+//		ModelAndView mav = new ModelAndView(); 
+		
+//		mav.setViewName("/farmdiary/farmdiarydetail");
+//		mav.addObject("data", farmdiary);
+		
+//		if(data.get("farmdiary") != null) {
+//			
+//		}else {
+//			mav.addObject("alertMsg", "해당 게시물이 존재하지 않습니다");
+//			mav.addObject("back", "back");
+//			mav.setViewName("common/result");
+//		}
+		
+//		return mav;
+		return "farmdiary/farmdiarydetail";
+	}
+	
+
+//	
+//	@RequestMapping("diary/diaryboardupload.do")
+//	public ModelAndView writeUpload() {
 //		
 //		ModelAndView mav = new ModelAndView();
 //		
 //		return mav;
+//		
 //	}
+	
 //	
-//	@RequestMapping("farmdiary/farmdiarydetail.do")
-//	public ModelAndView farmdiaryDetail() {
-//		
-//		ModelAndView mav = new ModelAndView(); 
-//		
-//		return mav;
-//	}
 //	
-//	@RequestMapping("farmdiary/farmdiarywrite.do")
-//	public String farmdiaryWrite() {
-//		return null;
-//		
-//	}
 //	
 //	@RequestMapping("farmdiary/farmdiarymodify")
 //	public void diaryModify() {
 //		
 //	}
 //	
-//	@RequestMapping("farmdiary/farmdiarylist.do")
+//	@RequestMapping("/farmdiary/farmdiarylist.do")
 //	public ModelAndView farmdiaryModify() {
 //		
 //		ModelAndView mav = new ModelAndView(); 
