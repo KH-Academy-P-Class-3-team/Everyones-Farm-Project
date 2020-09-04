c6801ee91bb31e5afc5b47137edf32355a73ea6b
YunTaeSeong <65264610+YunTaeSeong@users.noreply.github.com>
2020-09-04 15:15:55 +0900

    create review (#51)
    Co-authored-by: hyer0705 <hyer1205@gmail.com>

--- src/main/java/com/kh/farmapp/foodsales/model/dao/ReviewDaoImpl.java
+++ src/main/java/com/kh/farmapp/foodsales/model/dao/ReviewDaoImpl.java
@@ -1,67 +1,62 @@
 package com.kh.farmapp.foodsales.model.dao;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import common.dto.Product;
+import common.dto.TBOrder;
 import common.dto.Review;
 import common.dto.ReviewFile;
-import common.dto.TBOrder;
 import common.dto.UserTB;
 
-
 @Repository
-public class ReviewDaoImpl implements ReviewDao{
+public class ReviewDaoImpl implements ReviewDao {
 
-	@Override
-	public int updateReview(UserTB user, Review review) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Autowired
+    private SqlSessionTemplate sqlSession;
 
-//	@Override
-//	public List<Map<String, Object>> selectReview(Review review, User user, Product product) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
+    @Override
+    public int writeReview(HashMap map) {
 
-	@Override
-	public List<Map<String, Object>> reviewMore(Review review, ReviewFile reviewFile, UserTB user, Product product) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int writeReview(Review review, Product product) {
-		
-		// 클래스 다이어그램 용 DTO 객체 선언
-		TBOrder o = new TBOrder();
-		
-		return 0;
-	}
-
-	@Override
-	public int deleteReview(Review review) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	
+        // 클래스 다이어그램용 DTO 객체 선언
+        Review r = new Review();
+        ReviewFile rf = new ReviewFile();
+        UserTB user = new UserTB();
+        TBOrder o = new TBOrder();
 
 
+//        sqlSession.selectList()
+        int seq =  sqlSession.insert("Review.writeReview", map);
 
-	
-	
+        if(seq > 0){
+            return sqlSession.insert("Review.insertReviewFile", map);
+        }
+        return -1;
+    }
 
-	
-	
-	
+    @Override
+    public int deleteReview(Map map) {
+        sqlSession.delete("Review.deleteReview", map);
+        return sqlSession.delete("Review.deleteReview_file", map);
+    }
 
+    @Override
+    public String updateReview(Review review) {
+        return null;
+    }
 
+    @Override
+    public List selectReview(Map<String, Object> map) {
+        return sqlSession.selectList("Review.selectReviews", map);
+    }
 
-	
-	
-	
+    @Override
+    public List reviewMore(Map<String, Object> map) {
+        return null;
+    }
+
 }
