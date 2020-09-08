c2e5a0300881a5c6ad53fc6582caa4cd8bf3aabc
hyer0705 <hyer1205@gmail.com>
2020-09-08 10:23:22 +0900

    create admin member list view

--- src/main/java/com/kh/farmapp/admin/user/controller/AdminUserController.java
+++ src/main/java/com/kh/farmapp/admin/user/controller/AdminUserController.java
@@ -24,56 +24,56 @@ public class AdminUserController {
 	private AdminUserService adminUserService;
 	
 	// 농업인 회원 관리 페이지
-	@RequestMapping(value = "/useradmin/farmerlist", method = RequestMethod.GET)
+	@RequestMapping(value = "/adminmember/farmerlist", method = RequestMethod.GET)
 	public String adminFarmerList() {
 		return "";
 	}
 	
 	// 일반 회원 관리 페이지
-	@RequestMapping(value = "/useradmin/userlist", method = RequestMethod.GET)
+	@RequestMapping(value = "/adminmember/userlist", method = RequestMethod.GET)
 	public String adminUserList() {
 		return "admin/member/admin_user_list";
 	}
 	
 	// 농업인 회원 신청 관리 페이지
-	@RequestMapping(value = "/useradmin/fapplicationlist", method = RequestMethod.GET)
+	@RequestMapping(value = "/adminmember/fapplicationlist", method = RequestMethod.GET)
 	public String adminFarmerApplicationList() {
 		return "";
 	}
 	
 	// 농업인 회원 신청 관리 상세 페이지
-	@RequestMapping(value = "/useradmin/fapplicationdetail", method = RequestMethod.GET)
+	@RequestMapping(value = "/adminmember/fapplicationdetail", method = RequestMethod.GET)
 	public String adminFarmerApplicationDetail() {
 		return "";
 	}
 	
 	// 농업인 회원 신청 승인
-	@RequestMapping(value = "/useradmin/approvefapplication", method = RequestMethod.POST)
+	@RequestMapping(value = "/adminmember/approvefapplication", method = RequestMethod.POST)
 	public void approveFarmerApplication(Farmer farmer) {
 	}
 	
 	// 농업인 회원 신청 보류
-	@RequestMapping(value = "/useradmin/putfapplicationonhold", method = RequestMethod.POST)
+	@RequestMapping(value = "/adminmember/putfapplicationonhold", method = RequestMethod.POST)
 	public void putFarmerApplicationOnHold(Farmer farmer) {
 	}
 	
 	// 일반 회원 정지
-	@RequestMapping(value = "/useradmin/pauseuser", method = RequestMethod.POST)
+	@RequestMapping(value = "/adminmember/pauseuser", method = RequestMethod.POST)
 	public void pauseUser(UserTB user) {
 	}
 	
 	// 일반 회원 탈퇴
-	@RequestMapping(value = "/useradmin/deleteuser", method = RequestMethod.POST)
+	@RequestMapping(value = "/adminmember/deleteuser", method = RequestMethod.POST)
 	public void deleteUser(UserTB user) {
 	}
 	
 	// 농업인 회원 정지
-	@RequestMapping(value = "/useradmin/pausefarmer", method = RequestMethod.POST)
+	@RequestMapping(value = "/adminmember/pausefarmer", method = RequestMethod.POST)
 	public void pauseFarmer(Farmer farmer) {
 	}
 	
 	// 농업인 회원 탈퇴
-	@RequestMapping(value = "/useradmin/deletefarmer", method = RequestMethod.POST)
+	@RequestMapping(value = "/adminmember/deletefarmer", method = RequestMethod.POST)
 	public void deleteFarmer(Farmer farmer) {
 	}
 	
--- src/main/webapp/WEB-INF/views/admin/member/admin_user_list.jsp
+++ src/main/webapp/WEB-INF/views/admin/member/admin_user_list.jsp
@@ -8,8 +8,24 @@
 <%@include file="../include/admin_header.jsp" %>
 
 <div class="wrapper">
-	<main class="user-list__main">
-	<h1>Admin User List Page</h1>
+	<main class="member-list__main">
+		<div class="member-list__title">
+			<span class="member-list__title-text">일반 회원 관리</span>
+		</div>
+		<!-- 일반 회원 목록 -->
+		<div class="member__table">
+			<table class="member__m-table checkbox_group">
+				<tr class="m-table__tr-head">
+					<th class="m-table__th"><input type="checkbox" name="check-master" id="check_all" /></th>
+					<th class="m-table__th">아이디</th>
+					<th class="m-table__th">이름</th>
+					<th class="m-table__th">전화번호</th>
+					<th class="m-table__th">이메일</th>
+					<th class="m-table__th">가입날짜</th>
+				</tr>
+				
+			</table>
+		</div>
 	</main>
 </div>
 
